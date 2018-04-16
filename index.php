cb5b26428a3a4e385dbc7f242b41296f47219f28
G Cordova <gcordovaone@gmail.com>
2018-04-15 18:10:31 -0600

    created persona

--- index.php
+++ index.php
@@ -4,8 +4,20 @@
 		<title>datadesign</title>
 	</head>
 	<body>
+		<h1>Front End</h1>
 		<div>
-
+			<h2>Persona: User</h2>
+			<ul>
+				<li>Name: Art Vandalay</li>
+				<li>Gender: Male</li>
+				<li>Age: 22</li>
+				<li>Device: IPhone 7/iOS 11.2.6</li>
+				<li>Art is very confident is his use ability on his device. This is due to him being "glued" to his phone a high majority of his average day.</li>
+				<li>Art is also very motivated to participate in forums or blogs, due to the fact that he is very vocal about his opinions.</li>
+				<li>After participating in the blog, he feels satisfied to have the ability to have a platform to voice his opinion(s).</li>
+				<li>Art chooses to participate in this specific blog because he knows the integrity and reputation the website holds, due to it's "taste level".</li>
+				<li>Art also participates in other blogs as well. However, he returns to Hypebest frequently because of it's consistancy in the quality of posts.</li>
+			</ul>
 		</div>
 	</body>
 </html>
