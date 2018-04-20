25885d1ec0cb9e305db92d6467bc4e6e9312e424
G Cordova <gcordovaone@gmail.com>
2018-04-19 21:18:56 -0600

    figured out how to correctly set mutators, made adjustments

--- php/classes/Hype.php
+++ php/classes/Hype.php
@@ -53,82 +53,79 @@ class user {
 	public function getUserId() : string {
 		return $this->userId;
 	}
-
-
-
 	/**
 	 * mutator method for user id
 	 * @param  Uuid|string $newUserId new vallue of user id
 	 * @throws /range exception if $newUserId is not positive
 	 * @throws /typeerror if $newUserId is not an integer
 	 */
-	public function setUserId(string $newUserId) : string {
-		$this->userId = $userId;
+	private function setUserId($newUserName) : string {
+		$this->userId = $userId=$newUserName;
 	}
 
+	/**
+* mutator method for activation code
+* @param mixed $userActivationCode
+*/
+	public function setUserActivationCode($newuserActivationCode) : string {
+		$this->userActivationCode = $newuserActivationCode;
+	}
 	/**
 	 * accessor method for activation code
 	 * @return string of activation code
 	 */
-	public function getUserActivationCode() {
+	public function getUserActivationCode() : string {
 		return $this->userActivationCode;
 	}
 
-	/**
-	 * mutator method for activation code
-	 * @param mixed $userActivationCode
-	 */
-	public function setUserActivationCode($userActivationCode) {
-		$this->userActivationCode = $userActivationCode;
-	}
 
-	/**
-	 * accessor method for user email
-	 * @return string for user email
-	 */
-	public function getUserEmail() {
-		return $this->userEmail;
-	}
 
 	/**
 	 * mutator method for user email
 	 * @param mixed $userEmail
 	 */
-	public function setUserEmail($userEmail) {
-		$this->userEmail = $userEmail;
+	public function setUserEmail($newUserEmail) : string {
+		$this->userEmail = $newUserEmail;
+	}
+	/**
+	 * accessor method for user email
+	 * @return string for user email
+	 */
+	public function getUserEmail() : string {
+		return $this->userEmail;
 	}
 
-	/**
-	 * accessor method for user name
-	 * @return string of user name
-	 */
-	public function getUserName() {
-		return $this->userName;
-	}
 
 	/**
 	 * mutator method for user name
 	 * @param mixed $userName
 	 */
-	public function setUserName($userName) {
-		$this->userName = $userName;
+	public function setUserName($newUserName) : string {
+		$this->userName = $newUserName;
+	}
+	/**
+	 * accessor method for user name
+	 * @return string of user name
+	 */
+	public function getUserName() : string {
+		return $this->userName;
 	}
 
+
 	/**
+	 * mutator method for user hash
+	 * @param mixed $userHash
+	 */
+	public function setUserHash($newUserHash) : string {
+		$this->userHash = $newUserHash;
+	}
+}
+/**
 	 * accessor method for user hash (pw)
 	 * @return mixed of user hash (pw)
 	 */
 	public function getUserHash() {
 		return $this->userHash;
 	}
-
-	/**
-	 * mutator method for user hash
-	 * @param mixed $userHash
-	 */
-	public function setUserHash($userHash) {
-		$this->userHash = $userHash;
-	}
-}
 
 ?>
