68466ff212543278637d608bb99e4f3e73bd0e4f
G Cordova <gcordovaone@gmail.com>
2018-04-24 20:08:39 -0600

    created delete/insert statements on new file

--- sql/oml-assignment.sql
+++ sql/oml-assignment.sql
@@ -1,59 +1,12 @@
-/**
-insert statements
- */
+insert into post (postId, postAuthorId, postContent, postDateTime)
 
+insert into hype (hypeUserId, hypePostId)
 
-public function insert (/PDO $pdo) : void {
+insert into user (userId, userActivation code, userEmail, userHash, userName)
 
-$query = "INSERT INTO post(postId, postAuthorId, postContent, postDateTime) VALUES(:postId, :postAuthorId, :postContent, :postDateTime)";
-}
+insert into author (authorId, authorName)
 
-$statement = $pdo->prepare($query);
+delete post from post
 
-$formattedDate = $this->postDateTime->format(Y-m-d H:i:s.u);
-$parameters = ["postId" => $this->postId->getBytes(), "postAuthorId" => $this->postAuthorId, "postContent" => $this->postContent, "postDateTime" => $this->$formattedDate];
-$statement->execute($parameters);
-
-public function insert (/PDO $pdo) : void {
-
-$query = "INSERT INTO hype(hypeUserId, hypePostId) VALUES(:hypeUserId, :hypePostId)";
-}
-
-$statement = $pdo->prepare($query);
-
-	$formattedDate = $this->postDateTime->format(Y-m-d H:i:s.u);
-	$parameters = ["hypeUserId" => $this->"hypeUserId"->, "hypePostId" => $this->hypePostId];
-	$statement->execute($parameters);
-
-
-/**
-delete statements
- */
-
-delete(\PDO $pdo) : void {
-	$query = "DELETE FROM post WHERE postId = :postId";
-		$statement = $pdo->prepare($query);
-		$statement->execute($parameters);
-}
-
-delete(\PDO $pdo) : void {
-	$query = "DELETE FROM hype WHERE postId = :hypeUserId";
-		$statement = $pdo->prepare($query);
-		$statement->execute($parameters);
-}
-
-/**
-update statements
- */
-
-update(\PDO $pdo) : void {
-	$query = "UPDATE post SET postId = :postId, postContent = :postContent, postDateTime = :postDateTime postAuthorId = :postAuthorId";
-		$statement = $pdo->prepare($query);
-		}
-
-update(\PDO $pdo) : void {
-	$query = "UPDATE hype SET hypeId = :hypeId, hypeUserId = :hypeUserId";
-		$statement = $pdo->prepare($query);
-		}
-
+delete user from user
 
