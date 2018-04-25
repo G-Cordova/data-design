31605221fa160286b11285754438920f7b68bf06
G Cordova <gcordovaone@gmail.com>
2018-04-25 16:22:27 -0600

    fixed insert statement errors

--- sql/datadesign.sql
+++ sql/datadesign.sql
@@ -3,17 +3,17 @@ ALTER DATABASE gcordova25 CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 DROP TABLE IF EXISTS hype;
 DROP TABLE IF EXISTS post;
 DROP TABLE IF EXISTS author;
-DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS endUser;
 
-CREATE TABLE user (
-userId BINARY(16) NOT NULL,
-userActivationCode CHAR(32),
-userEmail VARCHAR(128) NOT NULL,
-userHash CHAR(97) NOT NULL,
-userName VARCHAR(16) NOT NULL,
-UNIQUE(userName),
-UNIQUE(userEmail),
-PRIMARY KEY(userId)
+CREATE TABLE endUser (
+endUserId BINARY(16) NOT NULL,
+endUserActivationCode CHAR(32),
+endUserEmail VARCHAR(128) NOT NULL,
+endUserHash CHAR(97) NOT NULL,
+endUserName VARCHAR(16) NOT NULL,
+UNIQUE(endUserName),
+UNIQUE(endUserEmail),
+PRIMARY KEY(endUserId)
 );
 
 CREATE TABLE author (
@@ -27,7 +27,7 @@ CREATE TABLE post (
 postId BINARY(16) NOT NULL,
 postAuthorId BINARY(16) NOT NULL,
 postContent VARCHAR(2000),
-postDateTime VARCHAR (6) NOT NULL,
+postDateTime DATETIME (6) NOT NULL,
 INDEX(postAuthorId),
 FOREIGN KEY(postAuthorId) REFERENCES author(authorId),
 PRIMARY KEY(postId)
@@ -36,10 +36,10 @@ PRIMARY KEY(postId)
 CREATE TABLE hype (
 	hypeUserId BINARY(16) NOT NULL,
 	hypePostId BINARY(16) NOT NULL,
-	FOREIGN KEY(hypePostId) REFERENCES post(postId),
-	FOREIGN KEY(hypeUserId) REFERENCES user(userId),
 	INDEX(hypeUserId),
 	INDEX(hypePostId),
+	FOREIGN KEY(hypePostId) REFERENCES post(postId),
+	FOREIGN KEY(hypeUserId) REFERENCES endUser(endUserId),
 	PRIMARY KEY(hypeUserId, hypePostId)
 );
 
--- sql/oml-assignment.sql
+++ sql/oml-assignment.sql
@@ -1,10 +1,13 @@
+INSERT into author (authorId, authorName)
+VALUES (UNHEX(REPLACE('5c7ecd33-7df7-4215-aa44-7d2c2b7a3672','-','')), 'authorxyz');
+
 insert into post (postId, postAuthorId, postContent, postDateTime)
-values (UNHEX(REPLACE('9defe769-4b42-4a1c-a3bc-15a6b1e2cfc6', '-', '')), (UNHEX(REPLACE('3489589f-d33f-41e6-b294-a1b20bd19a68', '-', '')), 'stringxyz', '2018-01-17 10:52:30.651024');
+values (UNHEX(REPLACE('9defe769-4b42-4a1c-a3bc-15a6b1e2cfc6','-','')), UNHEX(REPLACE('3489589f-d33f-41e6-b294-a1b20bd19a68','-','')), 'stringxyz', '2018-01-17 10:52:30.651024');
 
 insert into hype (hypeUserId, hypePostId)
 values (hypeUserId, hypePostId);
 
-delete post from post
+DELETE post from post
 
 delete user from user
 
